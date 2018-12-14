3b70a427b71f65452d6371782f9402d651bf6b6d
santosh100 <sahu18456@gmail.com>
2018-12-14 23:53:39 +0530

    second commit

--- demo.c
+++ demo.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
+#include<conio.h>
 int main()
 {
+	clrscr();
 	printf("\n hello this is santosh sahu");
+	printf("\n santosh do you really believe you are a progrmmer")
 	return 0;
 }
